17422105c0510766af55a3ded1b0772520e3757d
ottoabreu <ottoabreu@gmail.com>
2014-02-25 14:27:13 +0100

    test for ups parser chain

---
+++ ATMMonitoring/src/test/java/com/ncr/ATMMonitoring/test/NotParser.java
@@ -0,0 +1,12 @@
+package com.ncr.ATMMonitoring.test;
+
+import com.ncr.ATMMonitoring.parser.annotation.UPSParser;
+
+@UPSParser
+public class NotParser {
+
+	public NotParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ATMMonitoring/src/test/java/com/ncr/ATMMonitoring/test/ParserAbstractTest.java
@@ -0,0 +1,10 @@
+package com.ncr.ATMMonitoring.test;
+
+import com.ncr.ATMMonitoring.parser.ParseUPSXML;
+import com.ncr.ATMMonitoring.parser.annotation.UPSParser;
+@UPSParser
+public abstract class ParserAbstractTest extends ParseUPSXML {
+
+
+
+}
